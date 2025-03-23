83784610d38e168df5eb0d009f0a86722053f790
KIM TAEEUN <snowykte0426@naver.com>
2025-03-23 13:47:13 +0900

    add findOrderById endpoint to OrderController for retrieving orders by ID

--- src/main/java/com/gsm/_8th/class4/backend/task28/domain/order/controller/OrderController.java
+++ src/main/java/com/gsm/_8th/class4/backend/task28/domain/order/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.gsm._8th.class4.backend.task28.domain.order.controller;
 
+import com.gsm._8th.class4.backend.task28.domain.order.controller.data.OrderDto;
 import com.gsm._8th.class4.backend.task28.domain.order.controller.data.SearchOrderResponse;
+import com.gsm._8th.class4.backend.task28.domain.order.service.FindOrderByIdService;
 import com.gsm._8th.class4.backend.task28.domain.order.service.SearchOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final SearchOrderService searchOrderService;
+    private final FindOrderByIdService findOrderByIdService;
 
     @GetMapping("/search")
     public ResponseEntity<SearchOrderResponse> searchOrder(
@@ -28,5 +31,12 @@ public class OrderController {
             @RequestParam("size") int size
     ) {
         return ResponseEntity.status(HttpStatus.OK).body(searchOrderService.execute(userId, minPrice, maxPrice, address, itemName, page, size));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDto> findOrderById(
+            @RequestParam("orderId") Long orderId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(findOrderByIdService.execute(orderId));
     }
 }
